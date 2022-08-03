f7d11512a8b3dc8285c135ed9c3923f2c41a72c7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-03 10:52:30 +0530

    initial commit

---
+++ primitivetypes.js
@@ -0,0 +1,19 @@
+// typeof operator
+var v;
+typeof v;
+// Output : 'undefined'
+v = "1";
+typeof v;
+// Output : 'string'
+v = 2;
+typeof v;
+// Output : 'number'
+v = true;
+typeof v;
+// Output : 'boolean'
+v = {};
+typeof v;
+// Output : 'object'
+v = Symbol();
+typeof v;
+// Output : 'symbol'
