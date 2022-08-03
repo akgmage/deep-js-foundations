d44d375310aea1b48834699aac4b77dbc3e09891
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-03 10:58:59 +0530

    add some more typeof

--- primitivetypes.js
+++ primitivetypes.js
@@ -17,3 +17,14 @@ typeof v;
 v = Symbol();
 typeof v;
 // Output : 'symbol'
+typeof doesntExist;
+// Output : 'undefined'
+var v = null;
+typeof v;
+// Output : 'object'
+v = function () {};
+typeof v;
+// Output : 'function'
+v = [1, 2, 3];
+typeof v;
+// Output : 'object'
