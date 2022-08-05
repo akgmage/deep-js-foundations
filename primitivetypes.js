9ad57bcb58c42cb6f59c6def7231678ee422aa77
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-05 23:36:21 +0530

    add fix for Math.sign returning -0 and 0 instead for -1 and 1

--- primitivetypes.js
+++ primitivetypes.js
@@ -85,8 +85,28 @@ trendRate < 0;
 trendRate > 0;
 // Output : false
 
-// Built in checker to test for negative zero
-Object.is(trebdRate, -0);
+Object.is(trendRate, -0);
 // Output : true
-Object.is(trebdRate, 0);
+Object.is(trendRate, 0);
 // Output : false
+
+Math.sign(-3);
+// Output : -1
+Math.sign(3);
+// Output : 1
+Math.sign(-0);
+// Output : -0
+Math.sign(0);
+// Output : 0
+
+function sign(v) {
+  return v !== 0 ? Math.sign(v) : Object.is(v, -0) ? -1 : 1;
+}
+sign(-3);
+// Output : -1
+sign(3);
+// Output : 1
+sign(-0);
+// Output : -1
+sign(0);
+// Output : 1
