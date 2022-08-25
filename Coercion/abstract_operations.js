ddd57652eb2d4fa44963474909504eb5e848699a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-25 23:17:12 +0530

    add abstract operations

---
+++ Coercion/abstract_operations.js
@@ -0,0 +1,10 @@
+// ToPrimitive(hint)
+
+// hint: "number"
+// invoke valueOf() and see what it gives us, if it gives primitive , then we're done
+// if it doesn't give us a primitive or it doesn't exist, then we try the toString()
+// toString()
+
+// hint: "string"
+// toString()
+// valueOf()
