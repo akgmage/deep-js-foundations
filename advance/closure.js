8da4707c1a5455c7a4057579bb99bc1d4236be04
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-25 19:25:40 +0530

    add function constructor

--- advance/closure.js
+++ advance/closure.js
@@ -16,3 +16,19 @@ close();
 // advantages of closures
 // used in module patterns
 // data hiding and encapsulation
+
+function Counter() {
+  var count = 0;
+  this.incrementCounter = function () {
+    count++;
+    console.log(count);
+  };
+  this.decrementCounter = function () {
+    count--;
+    console.log(count);
+  };
+}
+var counter1 = new Counter();
+counter1.incrementCounter();
+counter1.incrementCounter();
+counter1.decrementCounter();
