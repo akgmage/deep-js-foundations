cd46d4f6b79bfd002dedd3b1048a94cdb0841d9a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-27 00:11:33 +0530

    add example for student grades

--- Arrays/filter.js
+++ Arrays/filter.js
@@ -1,3 +1,15 @@
+// The filter() method takes each element in an array and it applies a conditional statement against it.
+//If this conditional returns true, the element gets pushed to the output array.
 const numbers = [1, 2, 3, 5, 5, 8, 8, 10, 4];
 const evens = numbers.filter((x) => !(x & 1));
 console.log(evens);
+
+const students = [
+  { name: "Quincy", grade: 96 },
+  { name: "Lorem", grade: 84 },
+  { name: "Ipsum", grade: 99 },
+  { name: "Ador", grade: 66 },
+  { name: "Amat", grade: 76 },
+];
+const studentGrades = students.filter((x) => x.grade > 83);
+console.log(studentGrades);
