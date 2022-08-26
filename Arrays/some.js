e5c7d02bc70dab645095debe9ead7045f01dd610
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-27 00:45:39 +0530

    add some method

--- Arrays/some.js
+++ Arrays/some.js
@@ -1,3 +1,4 @@
+// The some() method tests whether at least one element in the array passes the test implemented by the provided function
 const hasNegativeNumbers = [1, 2, -3, 1, 4].some(function (item) {
   return item < 0;
 });
