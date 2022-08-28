2f56c9f4dd4f8e882ec983b9b4a4327873046a7c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-28 14:53:17 +0530

    add observer with next error and complete

--- RxJs/index.js
+++ RxJs/index.js
@@ -3,3 +3,14 @@ const { Observable } = require("rxjs");
 const observable = new Observable((subscriber) => {
   subscriber.next(10);
 });
+const observer = {
+  next: (value) => {
+    console.log("Observer got a value of " + value);
+  },
+  error: (err) => {
+    console.log("Observer got an error of " + err);
+  },
+  complete: () => {
+    console.log("Observer got a complete notification");
+  },
+};
