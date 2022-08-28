da660edadc26c83a14a764017d676cbc98c58056
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-28 16:22:40 +0530

    add user object

--- RxJs/index.js
+++ RxJs/index.js
@@ -1,4 +1,14 @@
 const { Observable } = require("rxjs");
+const users = {
+  data: [
+    { status: "active", age: 14 },
+    { status: "active", age: 13 },
+    { status: "inactive", age: 17 },
+    { status: "active", age: 32 },
+    { status: "inactive", age: 11 },
+    { status: "inactive", age: 78 },
+  ],
+};
 // Emit value of 10 from observable
 const observable = new Observable((subscriber) => {
   subscriber.next(10);
