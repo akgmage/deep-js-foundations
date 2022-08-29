b611375ec9c4989d7c0cfa13bb3cd70349e9c55e
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-29 22:57:27 +0530

    add reduce example

--- Arrays/reduce.js
+++ Arrays/reduce.js
@@ -17,3 +17,31 @@ const petCounts = pets.reduce(function (obj, pet) {
   return obj;
 }, {});
 console.log(petCounts);
+
+const pilots = [
+  {
+    id: 10,
+    name: "Poe Dameron",
+    years: 14,
+  },
+  {
+    id: 2,
+    name: "Temmin 'Snap' Wexley",
+    years: 30,
+  },
+  {
+    id: 41,
+    name: "Tallissan Lintra",
+    years: 16,
+  },
+  {
+    id: 99,
+    name: "Ello Asty",
+    years: 22,
+  },
+];
+const totalYears = pilots.reduce((value, pilot) => {
+  return value + pilot.years;
+}, 0);
+
+console.log(totalYears);
