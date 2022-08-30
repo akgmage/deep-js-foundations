6c57e5bd26a67e4bb7a5f7fecc5e81edb807d3d6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-08-30 23:00:32 +0530

    add map example

--- Arrays/map.js
+++ Arrays/map.js
@@ -2,3 +2,13 @@
 const numbers = [1, 2, 3, 4];
 const doubled = numbers.map((x) => x * 2);
 console.log(doubled);
+
+const users = [
+  { firstName: "Tipu", lastName: "To" },
+  { firstName: "Sultan", lastName: "Long" },
+  { firstName: "Bhagat", lastName: "White" },
+  { firstName: "Singh", lastName: "Red" },
+  { firstName: "Jai", lastName: "Blue" },
+];
+const concatNames = users.map((item) => `${item.firstName} ${item.lastName}`);
+console.log(concatNames);
