7ccbc5cfc5067041161ac6fbbde141e1ea37c4a4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-01 23:09:36 +0530

    add every example

--- Arrays/every.js
+++ Arrays/every.js
@@ -6,3 +6,11 @@ console.log(allPositive);
 const numbers = [1, 3, 5, 7, 9];
 const isOdd = numbers.every((item) => item & 1);
 console.log(isOdd);
+
+// test whether all elements in the numbers array is in the range specified by the min and max of the range object
+const nums = [1, 3, 15, 7];
+const range = { min: 0, max: 10 };
+const isInRange = nums.every(function (item) {
+  return item >= this.min && item <= this.max;
+}, range);
+console.log(isInRange);
--- Arrays/tempCodeRunnerFile.js
+++ Arrays/tempCodeRunnerFile.js
@@ -1,1 +1,2 @@
-const
+function (item) {
+  return
