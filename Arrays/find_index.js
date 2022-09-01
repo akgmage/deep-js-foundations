a31010bf5bbfda6b6973ac8f6baec428176d9acf
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-01 22:23:37 +0530

    add findIndex example

--- Arrays/find_index.js
+++ Arrays/find_index.js
@@ -2,3 +2,8 @@
 const objects = [{ id: "a" }, { id: "b" }, { id: "c" }];
 const foundIndex = objects.findIndex((item) => item.id === "c");
 console.log(foundIndex);
+
+// return the index of the first occurrence of the number 8 in the ranks array
+const ranks = [1, 5, 7, 8, 10, 7];
+const index = ranks.findIndex((rank) => rank === 8);
+console.log(index);
