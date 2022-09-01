1a8efded2ebf4f8962673b2b50f72df42bfaadb3
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-01 23:00:52 +0530

    add every example

--- Arrays/every.js
+++ Arrays/every.js
@@ -1,3 +1,7 @@
 // The every() method tests whether all elements in the array pass the test implemented by the provided function
 const allPositive = [1, -2, 3].every((item) => item > 0);
 console.log(allPositive);
+
+const numbers = [1, 3, 5, 7, 9];
+const isOdd = numbers.every((item) => item & 1);
+console.log(isOdd);
