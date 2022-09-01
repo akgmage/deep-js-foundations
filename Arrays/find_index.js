0dac0274b59d58a720826edc1014eab4a3724b33
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-01 22:53:31 +0530

    add findIndex example

--- Arrays/find_index.js
+++ Arrays/find_index.js
@@ -11,3 +11,12 @@ console.log(index);
 const ranks2 = [1, 5, 7, 8, 10, 7];
 const newIndex = ranks2.findIndex((rank, index) => rank === 7 && index > 2);
 console.log(newIndex);
+
+const products = [
+  { name: "Phone", price: 999 },
+  { name: "Computer", price: 1999 },
+  { name: "Tablet", price: 995 },
+];
+// find the index of the first product whose price is greater than 998
+const indexOfProduct = products.findIndex((product) => product.price > 998);
+console.log(indexOfProduct);
