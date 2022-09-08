892d77bc8768dbc321e17de250743dbb4fe8a055
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-08 22:54:41 +0530

    add map example

--- Arrays/map.js
+++ Arrays/map.js
@@ -20,3 +20,7 @@ arr.map((item, index, array) => {
   console.log(item, index, array);
   return item;
 });
+
+const bdays = ["12-12", "11-11", "01-01", "02-02"];
+const newBdays = bdays.map((item) => item.replace("-", "/"));
+console.log(newBdays);
