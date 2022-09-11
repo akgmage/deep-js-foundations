075e9902bdd0642bdaf11b196c2c395a2f67650f
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-11 21:06:55 +0530

    add comment

--- RxJs/push_asynchronous_values.js
+++ RxJs/push_asynchronous_values.js
@@ -1,11 +1,13 @@
 const { Observable } = require("rxjs");
 
 const observable = new Observable((subscriber) => {
+  // log a message every second
   const id = setInterval(() => {
     subscriber.next("test");
     console.log("leak");
   }, 1000);
   subscriber.complete();
+  // clear interval to prevent memory leak
   return () => {
     clearInterval(id);
   };
