9221b99adcffd00bfb8b05644fe1fe2b069a6df7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-11 21:35:05 +0530

    emit first value after 4 second and subsequent values every 1 seconds after

--- RxJs/operators.js
+++ RxJs/operators.js
@@ -1,7 +1,11 @@
 const { timer } = require("rxjs");
 
 // After given duration, emit numbers in sequence every specified duration
-
-const observable = new timer(0, 1000);
+/*
+ * timer takes a second argument, how often to emit subsequent values
+ * in this case we will emit first value after 4 second and subsequent
+ * values every 1 seconds after
+ */
+const observable = new timer(4000, 1000);
 
 const subscription = observable.subscribe(console.log);
