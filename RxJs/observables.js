31fa65839a8092aa597c5dbeca83a90919c75640
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-11 20:54:42 +0530

    add complete

--- RxJs/observables.js
+++ RxJs/observables.js
@@ -2,9 +2,12 @@ const { Observable } = require("rxjs");
 
 const observable = new Observable((subscriber) => {
   subscriber.next("Hello World");
+  subscriber.next("Hello World 2");
+  subscriber.complete();
 });
 observable.subscribe({
   next: (value) => {
     console.log(value);
   },
+  complete: () => console.log("Completed"),
 });
