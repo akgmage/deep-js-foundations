c68df76a5d79210cc4cdfb11ca0d6d2b36921de6
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-11 21:32:10 +0530

    add comment

--- RxJs/operators.js
+++ RxJs/operators.js
@@ -1,5 +1,7 @@
 const { timer } = require("rxjs");
 
+// After given duration, emit numbers in sequence every specified duration
+
 const observable = new timer(0, 1000);
 
 const subscription = observable.subscribe(console.log);
