d2ef694bdf5be0eb4456dd6bf1eab73a4630c2e2
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-12 23:20:07 +0530

    add comment

--- RxJs/take.js
+++ RxJs/take.js
@@ -2,6 +2,7 @@ const { interval } = require("rxjs");
 const { reduce, take } = require("rxjs/operators");
 
 const observable = interval(500).pipe(
+  //Emit provided number of values before completing.
   take(10),
   reduce((acc, item) => acc + item, 0)
 );
