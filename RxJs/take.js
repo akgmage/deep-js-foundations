8d7405baf729f5ee835ca5d119daaaa90d688cd7
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-12 23:19:36 +0530

    add take

---
+++ RxJs/take.js
@@ -0,0 +1,18 @@
+const { interval } = require("rxjs");
+const { reduce, take } = require("rxjs/operators");
+
+const observable = interval(500).pipe(
+  take(10),
+  reduce((acc, item) => acc + item, 0)
+);
+
+const subscription = observable.subscribe({
+  next(value) {
+    console.log(value);
+  },
+  complete() {
+    console.log("completed");
+  },
+});
+
+console.log("hello");
