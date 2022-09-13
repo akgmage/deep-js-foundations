9f1ac4a44e0dc98b209b5351adad8e671e88c622
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-13 22:30:10 +0530

    complete inner observable

--- RxJs/flattening_operators.js
+++ RxJs/flattening_operators.js
@@ -1,12 +1,12 @@
-const { fromEvent } = require("rxjs");
-const { map, mergeMap } = require("rxjs/operators");
+const { fromEvent, interval } = require("rxjs");
+const { map, mergeMap, tap } = require("rxjs/operators");
 const { ajax } = require("rxjs/ajax");
 // Emit numbers in sequence based on provided timeframe
 const observable = fromEvent(button, "click").pipe(
   // Map to observable, emit value
   mergeMap(() => {
     // Create an observable for an Ajax request with either a request object with url, headers, etc or a string for a URL.
-    return ajax.getJSON("https://jsonplaceholder.typicode.com/todos/1");
+    return interval(1000).pipe(tap(console.log), take(5));
   })
 );
 
