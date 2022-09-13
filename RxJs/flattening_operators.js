8cc8938045cd2b7e080e3412b249a41cdb8ae0f9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-13 22:35:39 +0530

    add switchmap

--- RxJs/flattening_operators.js
+++ RxJs/flattening_operators.js
@@ -1,12 +1,19 @@
 const { fromEvent, interval } = require("rxjs");
-const { map, mergeMap, tap } = require("rxjs/operators");
+const { map, switchMap, tap } = require("rxjs/operators");
 const { ajax } = require("rxjs/ajax");
 // Emit numbers in sequence based on provided timeframe
 const observable = fromEvent(button, "click").pipe(
-  // Map to observable, emit value
-  mergeMap(() => {
+  // Map to observable, complete previous inner observable, emit values
+  switchMap(() => {
     // Create an observable for an Ajax request with either a request object with url, headers, etc or a string for a URL.
-    return interval(1000).pipe(tap(console.log), take(5));
+    return interval(1000).pipe(
+      tap({
+        complete() {
+          console.log("inner observable has been completed");
+        },
+      }),
+      take(5)
+    );
   })
 );
 
