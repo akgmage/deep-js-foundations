1645fc19bd17b0abd8225ebc2a9c036bb37afa3c
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2022-09-13 22:44:12 +0530

    add map example rxjs

--- RxJs/map.js
+++ RxJs/map.js
@@ -1,4 +1,4 @@
-const { of } = require("rxjs");
+const { of, from } = require("rxjs");
 const { map } = require("rxjs/operators");
 
 // Apply projection with each value from source
@@ -14,3 +14,13 @@ const subscription = observable.subscribe({
 });
 
 console.log("hello");
+
+const dataSource = from([
+  { name: "Abhi", age: 20 },
+  { name: "John", age: 10 },
+  { name: "Dev", age: 45 },
+  { name: "Cya", age: 44 },
+]);
+// grab each persons name
+const example = dataSource.pipe(map(({ name }) => name));
+const subscribe = example.subscribe((val) => console.log(val));
